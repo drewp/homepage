54b54a990f98c3b7890c9ea5aa34fa0d7ae526ef
drewp <drewp@bigasterisk.com>
2016-01-28 00:06:49 -0800

    fix Map keys to be strings that do '==' right. more error log if remove(quad) fails.
    Ignore-this: b07fb6a2190d2f24001d46fda74ade2f
    
    darcs-hash:1a24a896da5fcb4f032e10067a29cf3b7c3f805e

--- www/rdf/quadstore.js
+++ www/rdf/quadstore.js
@@ -1,11 +1,20 @@
 // a more straightforward store than rdfstore.Store, which I haven't
 // figured out yet wrt quads.
 function QuadStore() {
+    this.ntLookup = new Map(); // ntstring: node
     this.index = new Map();  // {graphNt: {subjNt: {predNt: {objNt: true}}}}
 }
+
 QuadStore.prototype.clear = function() {
     this.index.clear();
 };
+
+QuadStore.prototype.ntKey = function(node) {
+    var nt = node.toNT();
+    this.ntLookup.set(nt, node);
+    return nt; // could return a small int, even
+}
+
 QuadStore.prototype.add = function(quad) {
     function setdefault(obj, k, default_) {
         var v = obj.get(k);
@@ -16,33 +25,42 @@ QuadStore.prototype.add = function(quad) {
         return v;
     }
     
-    var m = setdefault(this.index, quad.graph.toNT(), new Map());
-    m = setdefault(m, quad.subject.toNT(), new Map());
-    m = setdefault(m, quad.predicate.toNT(), new Map());
-    setdefault(m, quad.object.toNT(), true);
+    var m = setdefault(this.index, this.ntKey(quad.graph), new Map());
+    m = setdefault(m, this.ntKey(quad.subject), new Map());
+    m = setdefault(m, this.ntKey(quad.predicate), new Map());
+    setdefault(m, this.ntKey(quad.object), true);
 };
+
 QuadStore.prototype.remove = function(quad) {
-
-    function clean(parent, key, child) {
-        if (child.size == 0) {
-            parent.delete(key);
-            return true;
+    // doesn't care if the quad was in the graph or not
+    try {
+        function clean(parent, key, child) {
+            if (child.size == 0) {
+                parent.delete(key);
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-    var g = quad.graph.toNT();
-    var s = quad.subject.toNT();
-    var p = quad.predicate.toNT();
-    
-    var l1 = this.index.get(g);
-    var l2 = l1.get(s);
-    var l3 = l2.get(p);
-    l3.delete(quad.object.toNT());
-    if (clean(l2, p, l3)) {
-        if (clean(l1, s, l2)) {
-            clean(this.index, g, l1);
+        var g = this.ntKey(quad.graph);
+        var s = this.ntKey(quad.subject);
+        var p = this.ntKey(quad.predicate);
+        
+        var l1 = this.index.get(g);
+        if (l1 === undefined) { return; }
+        var l2 = l1.get(s);
+        if (l2 === undefined) { return; }
+        var l3 = l2.get(p);
+        if (l3 === undefined) { return; }
+        l3.delete(this.ntKey(quad.object));
+        if (clean(l2, p, l3)) {
+            if (clean(l1, s, l2)) {
+                clean(this.index, g, l1);
+            }
         }
+    } catch(e) {
+        console.log('while removing', quad, ':');
+        throw e;
     }
 };
 QuadStore.prototype.quads = function(onQuad) {
